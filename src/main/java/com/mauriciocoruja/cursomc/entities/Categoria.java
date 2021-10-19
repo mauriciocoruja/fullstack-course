2c04c87921d8a9ebbc750fded8ea92fd1d054a31
mauriciocoruja <mbjr@outlook.com.br>
2021-10-19 06:47:11 -0300

    Adicionado banco de dados H2 e criação automática da base de dados

--- src/main/java/com/mauriciocoruja/cursomc/entities/Categoria.java
+++ src/main/java/com/mauriciocoruja/cursomc/entities/Categoria.java
@@ -1,12 +1,17 @@
 package com.mauriciocoruja.cursomc.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Stack;
 
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
