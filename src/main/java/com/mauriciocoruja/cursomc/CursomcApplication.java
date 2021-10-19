a50d25f51d3ea00a0459915f42b2bc9650fc4d67
mauriciocoruja <mbjr@outlook.com.br>
2021-10-19 09:03:13 -0300

    Criada a operação de instanciação dos objetos no banco de dados

--- src/main/java/com/mauriciocoruja/cursomc/CursomcApplication.java
+++ src/main/java/com/mauriciocoruja/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.mauriciocoruja.cursomc;
 
+import com.mauriciocoruja.cursomc.domain.entities.Categoria;
+import com.mauriciocoruja.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+
 	}
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+
+	}
 }
